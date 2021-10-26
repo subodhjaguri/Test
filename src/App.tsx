52c1e35d0a261aecdadad14a9f395f7084209daa
subodhjaguri <subodhjaguri1@gmail.com>
2021-10-26 19:34:42 +0530

    test completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Dropdown, Image, Row } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+  const [currency, setCurrency] = useState("INR");
+  const [rate, setRate] = useState(1);
+
+  const products = [
+    {
+      id: 1,
+      image: "/images/car1.jpeg",
+      name: "product1",
+      price: 200,
+    },
+    {
+      id: 2,
+      image: "images/car2.jpeg",
+      name: "product2",
+
+      price: 500,
+    },
+    {
+      id: 3,
+      image: "images/lw.jpeg",
+      name: "product3",
+
+      price: 800,
+    },
+  ];
+  const getExchangeRate = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_EXCHAGE_API_KEY}/latest/INR`
+      );
+      if (data) {
+        // console.log(data?.conversion_rates.USD);
+        setRate(data?.conversion_rates.USD);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getExchangeRate();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <h1 style={{ textAlign: "center" }}>Exercise </h1>
+      <div
+        style={{
+          border: "1px solid grey",
+          display: "flex",
+          justifyContent: "space-between",
+          width: "90%",
+          margin: "auto",
+          padding: "2% 1%",
+          borderRadius: "10px",
+          flexWrap: "wrap",
+        }}
+      >
+        {products.map((product) => (
+          <div style={{ width: "25%", height: "30%" }}>
+            <Image
+              style={{ width: "100%", height: "100px" }}
+              src={product.image}
+              rounded
+              fluid
+            />
+            <div style={{ textAlign: "center" }}>
+              <p> {product.name}</p>
+              <p>
+                {" "}
+                {currency == "INR"
+                  ? ` ₹ ${product.price.toFixed(2)} `
+                  : `$ ${(rate * product.price).toFixed(2)}`}
+              </p>
+            </div>
+          </div>
+        ))}
+        <Dropdown style={{ width: "20%" }}>
+          <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+            Currency :{currency == "INR" ? "INR" : "USD"}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => setCurrency("INR")}>
+              INR
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => setCurrency("USD")}>
+              USD
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </>
   );
 }
 
